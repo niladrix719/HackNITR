a6b63af9f441d1c04a72192526caef3bdc2b7979
SUBID DAS <subid.das@awesomesuite.com>
2024-03-02 13:41:43 +0530

    Refactor hero component and update dependencies

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
-
 const Hero = () => {
-	return (
-		<>
-			<div className='w-full h-screen bg-hero-pattern bg-cover bg-center'></div>
-		</>
-	);
+  return (
+    <>
+      <div className="bg-hero-pattern h-screen w-full bg-cover bg-center"></div>
+    </>
+  );
 };
 
 export default Hero;
