d6a9460a3c848e82ba700ac2934613cebf584025
SUBID DAS <subid.das@awesomesuite.com>
2024-03-02 13:54:55 +0530

    Update MongoDB connection and add socket.io-client dependency

--- server/mongoDB.js
+++ server/mongoDB.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const { MONGO_URI } = process.env;
+require("dotenv").config();
+const mongoose = require("mongoose");
+const { MONGO_URI = "mongodb://localhost:27017" } = process.env;
 
 const connectDb = async () => {
   try {
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is missing");
+    }
     await mongoose.connect(MONGO_URI);
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
   }
 };
 
