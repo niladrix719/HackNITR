60ab1adeb43ee16311c7f5f87cab81f66ddc6b82
SUBID DAS <subid.das@awesomesuite.com>
2024-03-02 13:49:43 +0530

    Delete unused files and update Navbar component

--- client/src/Code.jsx
+++ client/src/Code.jsx
@@ -53,6 +53,10 @@ const CodeEditor = ({ socket }) => {
 
     const navbarBottom = navbar.offsetTop + navbar.offsetHeight;
     const mainSectionTop = mainSection.offsetTop;
+    console.log({
+      navbarBottom,
+      mainSectionTop
+    })
     if (navbarBottom <= mainSectionTop) {
       setTestPassed((prev) => prev + 1);
     }
@@ -106,7 +110,7 @@ const CodeEditor = ({ socket }) => {
           <div className="html flex flex-col">
             <h2 className="relative z-10 bg-black px-4 py-2 text-white">
               HTML Editor
-              {testPassed}
+              { testPassed }
             </h2>
             <Editor
               defaultLanguage="html"
@@ -166,14 +170,14 @@ const CodeEditor = ({ socket }) => {
         </div>
         <div>
           {showResult && (
-            <div className="result fixed bottom-0 right-0 m-4 rounded-lg bg-white p-4 shadow-lg">
+            <div className="result fixed bottom-0 right-0 m-4 p-4 bg-white rounded-lg shadow-lg">
               <h2>Result</h2>
               <p>You Win!!</p>
               <p>Test cases passed: {testPassed}</p>
             </div>
           )}
           {!showResult && showResultLose && (
-            <div className="result fixed bottom-0 right-0 m-4 rounded-lg bg-white p-4 shadow-lg">
+            <div className="result fixed bottom-0 right-0 m-4 p-4 bg-white rounded-lg shadow-lg">
               <h2>Result</h2>
               <p>You Lose!!</p>
               <p>Test cases passed: {testPassed}</p>
@@ -182,12 +186,7 @@ const CodeEditor = ({ socket }) => {
         </div>
       </div>
       <div className="problem-statement container-custom my-8 rounded-md bg-slate-200 px-8 py-4">
-        <div className="mb-4 flex items-center gap-2">
-          <h2 className="text-2xl font-bold">238. Fix Navbar</h2>{" "}
-          <p className="inline-block rounded-3xl bg-green-300 px-6 py-1 text-green-900">
-            Easy
-          </p>
-        </div>
+        <div className="flex mb-4 gap-2 items-center"><h2 className="text-2xl font-bold">238. Fix Navbar</h2> <p className="py-1 inline-block rounded-3xl px-6 bg-green-300 text-green-900">Easy</p></div>
         <p>
           The navbar is colliding with the content.
           {/* <br></br> */}
--- client/src/components/hero.jsx
+++
@@ -1,9 +0,0 @@
-const Hero = () => {
-  return (
-    <>
-      <div className="bg-hero-pattern h-screen w-full bg-cover bg-center"></div>
-    </>
-  );
-};
-
-export default Hero;
